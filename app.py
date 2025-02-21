aed3a3758c8db6efb4a172427ccab65c54854803
Xsupremeswagx <raista080@gmail.com>
2025-02-20 21:55:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# Store location data and logs in memory
-locations = []
+# Store logs in memory
 logs = []
 
 @app.route('/')
@@ -13,26 +12,16 @@ def home():
     user_device = request.user_agent.string
     logs.append({"ip": user_ip, "device": user_device, "message": "Hello, visit /location to send your location."})
 
-    return render_template('index.html')  # Serve the HTML page
+    return render_template('index.html')  # Serve the animation HTML page
 
 @app.route('/location', methods=['POST'])
 def store_location():
     data = request.get_json()  # Parse the incoming JSON data
-
     latitude = data.get('latitude')
     longitude = data.get('longitude')
-    user_ip = request.remote_addr
-    user_device = request.user_agent.string
 
     if latitude and longitude:
-        locations.append({"latitude": latitude, "longitude": longitude, "ip": user_ip, "device": user_device})
-        return jsonify({
-            "message": "Location data received successfully!",
-            "latitude": latitude,
-            "longitude": longitude,
-            "ip": user_ip,
-            "device": user_device
-        }), 200
+        return jsonify({"message": "Location data received successfully!"}), 200
     else:
         return jsonify({"message": "Location data is missing!"}), 400
 
@@ -41,5 +30,4 @@ def get_logs():
     return jsonify(logs), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(debug=True)
